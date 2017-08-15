7ef0b35464050b02702399abb7f336bb2e200624
yasunori <yasunori@YASUNORI-PC>
2017-08-15 17:39:56 +0900

    test

---
+++ WebApplication1/Controllers/BeginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BeginController : Controller
+    {
+        private MvcBasicContext db = new MvcBasicContext();
+
+        /// <summary>
+        /// Listを返す
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List()
+        {
+            return View(db.Members);
+        }
+
+        // GET: Begin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
