0a93b35c5c1feac5c070e847c76e33b0dff1277c
yasunori <yasunori@YASUNORI-PC>
2017-08-15 16:31:10 +0900

    test

---
+++ WebApplication1/Models/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Member
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Birth { get; set; }
+        public bool Married { get; set; }
+        public string Memo { get; set; }
+    }
+}
